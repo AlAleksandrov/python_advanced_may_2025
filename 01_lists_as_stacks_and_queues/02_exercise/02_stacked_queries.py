db5d0849907797f6a881954beeabfd1ee2618c7c
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-05-18 10:20:22 +0300

    Update 02_stacked_queries.py

--- 01_lists_as_stacks_and_queues/02_exercise/02_stacked_queries.py
+++ 01_lists_as_stacks_and_queues/02_exercise/02_stacked_queries.py
@@ -3,20 +3,15 @@ stack = []
 
 for i in range(number):
     queries = input().split()
-    if len(queries) > 1:
+    if queries[0] == "1":
         stack.append(int(queries[1]))
-    elif len(queries) == 1:
+    elif stack:
         if queries[0] == "2":
-            if len(stack) > 0:
-                stack.pop()
+            stack.pop()
         elif queries[0] == "3":
             print(max(stack))
         elif queries[0] == "4":
             print(min(stack))
 
 
-for _ in range(len(stack)):
-    if len(stack) > 1:
-        print(stack.pop(), end=", ")
-    else:
-        print(stack.pop())
+print(", ".join([str(x) for x in reversed(stack)]))
