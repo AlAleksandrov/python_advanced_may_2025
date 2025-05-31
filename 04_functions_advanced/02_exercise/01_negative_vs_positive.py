2840f4b84153aa9abbbfef6bc8e57a162d68030c
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-05-31 20:04:59 +0300

    Update 01_negative_vs_positive.py

--- 04_functions_advanced/02_exercise/01_negative_vs_positive.py
+++ 04_functions_advanced/02_exercise/01_negative_vs_positive.py
@@ -1,7 +1,7 @@
 def separate_and_sum(*args):
     positive_sum = sum([x for x in args if x > 0])
     negative_sum = sum([x for x in args if x < 0])
-    
+
     if abs(negative_sum) > positive_sum:
         result = f"{negative_sum}\n{positive_sum}\nThe negatives are stronger than the positives"
     else:
