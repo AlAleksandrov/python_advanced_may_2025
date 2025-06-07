759e770be2ee08dc79520b2834785aeb7bb3b221
Aleksandar Aleksandrov <139697109+AlAleksandrov@users.noreply.github.com>
2025-06-07 17:27:46 +0300

    Update even_lines.py

--- 06_file_handling_exercise/01_even_lines/even_lines.py
+++ 06_file_handling_exercise/01_even_lines/even_lines.py
@@ -1,8 +1,11 @@
 with open("text.txt") as file:
+
     for index, line in enumerate(file):
         if index % 2 == 0:
             clean_line = line.strip()
+
             for ch in "-,.!?":
                 clean_line = clean_line.replace(ch, "@")
+
             reversed_words = " ".join(reversed(clean_line.split()))
             print(reversed_words)
